058bed7c53628627aef0049ac944cd360f4b3ae3
robinnasimhossain26 <robnnasimhossain@gmail.com>
2025-09-28 22:44:26 +0800

    add

--- config/db.js
+++ config/db.js
@@ -1,16 +1,30 @@
+// import mongoose from "mongoose";
+
+// const connectDB = async () => {
+//   try {
+//     if (!process.env.MONGO_URI) throw new Error("MONGO_URI not defined");
+//     await mongoose.connect(process.env.MONGO_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log("MongoDB connected successfully");
+//   } catch (err) {
+//     console.error(err);
+//     process.exit(1);
+//   }
+// };
+
+// export default connectDB;
 import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
     if (!process.env.MONGO_URI) throw new Error("MONGO_URI not defined");
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected successfully");
   } catch (err) {
-    console.error(err);
-    process.exit(1);
+    console.error("MongoDB connection error:", err.message);
+    process.exit(1); // exit server if DB not connected
   }
 };
 
